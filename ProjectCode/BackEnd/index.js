b46d1325417f55a78c4e64396a2087e03a1fc1cb
Alexis Serrano <aserrano031@github.com>
2023-04-07 18:24:45 -0500

    working database

--- ProjectCode/BackEnd/index.js
+++ ProjectCode/BackEnd/index.js
@@ -1,26 +1,32 @@
-const express = require("express");
-const app = express();
-const mysql = require("mysql");
+const express = require("express")
+const app = express()
+const mysql = require("mysql")
+
 var db = mysql.createConnection({
-    host:'34.66.235.118', //could be ip address of sql data - change if database does not change
-    user: 'root',
-    password:'test1234',
-    database:'Game_Data',
+  host: "34.66.235.118",
+  user: "root",
+  password: "test1234",
+  database: "Game_Data",
 })
 
 db.connect(function(err) {
     if (err) throw err;
-    console.log("Connected to database");
-});
+    // var sql = "INSERT INTO `LoginUser` (`UserId`,`FirstName`, `LastName`) VALUES (20, 'Zack', 'M');";
+    // var sql = "INSERT INTO `BannedGameTitle` (`UserId`, `GameId`) VALUES (10, 20);";
+    // var sql = "DELETE FROM BannedGameTitle WHERE UserId = 10";
+    db.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log(result.affectedRows + " record(s) updated");
+    });
+  });
 
-app.get('/', (require, response) => {
-    // const sqlInsert = "INSERT INTO `movie_reviews` (`movieName`,`movieReview`) VALUES ('Spider2', 'good movie');";
-    const sqlInsert = "INSERT INTO `BannedGameTitle` (`UserId`, `GameId`) VALUES (01, 20);";
-    db.query(sqlInsert, (err, result) => {
-        response.send("Insert success")
-    }) })
-
+// app.get('/', (require, response) => {
+//   const sqlInsert = "INSERT INTO `BannedGameTitle` (`UserId`, `GameId`) VALUES (10, 20);";
+//   db.query(sqlInsert, (err, result) => {
+//     response.send("Hello, Word!!!!");
+//   })
+// })
 
 app.listen(3002, () => {
-    console.log("running on port 3002");
+  console.log("running on port 3002")
 })
