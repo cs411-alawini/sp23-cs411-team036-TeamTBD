cf852a9c575b8b9bfa2529717c448c0dcaeeb997
Alexis Serrano <aserrano031@github.com>
2023-04-06 22:25:58 -0500

    connected database

--- ProjectCode/BackEnd/index.js
+++ ProjectCode/BackEnd/index.js
@@ -2,17 +2,22 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 var db = mysql.createConnection({
-    host:'localhost', //could be ip address of sql data - change if database does not change
+    host:'34.66.235.118', //could be ip address of sql data - change if database does not change
     user: 'root',
     password:'test1234',
     database:'Game_Data',
 })
 
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to database");
+});
+
 app.get('/', (require, response) => {
     // const sqlInsert = "INSERT INTO `movie_reviews` (`movieName`,`movieReview`) VALUES ('Spider2', 'good movie');";
-    const sqlInsert = "INSERT INTO `BannedGameTitle` (`UserId`, `GameId`) VALUES (01,20);";
+    const sqlInsert = "INSERT INTO `BannedGameTitle` (`UserId`, `GameId`) VALUES (01, 20);";
     db.query(sqlInsert, (err, result) => {
-        response.send("Hello world!!!!!!!!!!");
+        response.send("Insert success")
     }) })
 
 
